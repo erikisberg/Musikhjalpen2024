1a7502d3597c66dccd18f5b4697f60f8076cd488
Erik Isberg <erik@isberg.nu>
2024-11-30 22:30:28 +0100

    v1

---
+++ public/embed.js
@@ -0,0 +1,6 @@
+(function() {
+  var script = document.createElement('script');
+  script.src = 'https://musikhjalpen.adeprimo.com/funktion-widget.js';
+  script.async = true;
+  document.head.appendChild(script);
+})();
